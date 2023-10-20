13b523f223afeae3c88a44836a473d4be3615445
hwajiny <ghkwls02224@gmail.com>
2023-10-20 16:46:43 +0900

    [[FEAT] : ⚙️] : 구글로그인 기능 조정중

--- app/src/main/java/com/footprint/app/ui/login/LoginActivity.kt
+++ app/src/main/java/com/footprint/app/ui/login/LoginActivity.kt
@@ -1,44 +1,68 @@
 package com.footprint.app.ui.login
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.SignInButton
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.footprint.app.R
-import com.google.android.gms.auth.api.identity.BeginSignInRequest
-import com.google.android.gms.auth.api.identity.Identity
-import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.android.gms.common.api.ApiException
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var oneTapClient: SignInClient
-    private lateinit var signInRequest: BeginSignInRequest
+    private lateinit var auth: FirebaseAuth
+    private lateinit var btnGoogleSignIn: SignInButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        oneTapClient = Identity.getSignInClient(this)
-        signInRequest = BeginSignInRequest.builder()
-            .setPasswordRequestOptions(BeginSignInRequest.PasswordRequestOptions.builder()
-                .setSupported(true)
-                .build())
-            .setGoogleIdTokenRequestOptions(
-                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
-                    .setSupported(true)
-                    // Your server's client ID, not your Android client ID.
-                    .setServerClientId(getString(R.string.your_web_client_id))
-                    // Only show accounts previously used to sign in.
-                    .setFilterByAuthorizedAccounts(true)
-                    .build())
-            // Automatically sign in when exactly one credential is retrieved.
-            .setAutoSelectEnabled(true)
+        auth = FirebaseAuth.getInstance()
+        btnGoogleSignIn = findViewById(R.id.btngoogle)
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("796616173651-tuijqr0er165et5ln12lgud85gc39d8h.apps.googleusercontent.com")
+            .requestEmail()
             .build()
+        val mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
 
-
-        val signInButton = findViewById<SignInButton>(R.id.loginbtn)
-        signInButton.setOnClickListener {
-
-
+        // 이벤트 추가하기
+        btnGoogleSignIn.setOnClickListener {
+            val signInIntent = mGoogleSignInClient.signInIntent
+            startActivityForResult(signInIntent, RC_SIGN_IN)
         }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Firebase 로그인 성공시
+                } else {
+                    // Firebase 로그인 실패시
+                }
+            }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                // Google 로그인이 성공했을 때 Firebase에 인증
+                firebaseAuthWithGoogle(account?.idToken!!)
+            } catch (e: ApiException) {
+                // Google 로그인 실패시
+
+            }
+        }
+    }
+    companion object {
+        const val RC_SIGN_IN = 9001
     }
 }
